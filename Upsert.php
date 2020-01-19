e378b163689afa741eafb6f00fea5106b82debb6
Fabian Franz <github@fabian-franz.de>
2020-01-19 13:04:12 +0100

    Implement Upsert via Merge for now

--- Upsert.php
+++ Upsert.php
@@ -13,8 +13,38 @@ class Upsert extends QueryUpsert {
    * {@inheritdoc}
    */
   public function __toString() {
-    // @TODO: handle this?
-    return '';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute() {
+    if (!$this->preExecute()) {
+      return NULL;
+    }
+
+    foreach ($this->insertValues as $insert_values) {
+      $this->executeOne($insert_values);
+    }
+
+    // @todo Should be last insert id.
+    return NULL;
+  }
+
+  public function executeOne($insert_values) {
+    $combined = array_combine($this->insertFields, $insert_values);
+    $keys = [$this->key => $combined[$this->key]];
+
+    $merge = $this->connection
+      ->merge($this->table)
+      ->fields($combined)
+      ->keys($keys);
+
+    if ($this->defaultFields) {
+      $merge->useDefaults($this->defaultFields);
+    }
+
+    return $merge->execute();
   }
 
 }
