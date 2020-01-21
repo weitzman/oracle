539c1be1a778df3a495b28491f635b06734e9364
Fabian Franz <github@fabian-franz.de>
2020-01-21 18:20:58 +0100

    Workaround merge core bug: Duplicate column

--- Merge.php
+++ Merge.php
@@ -7,4 +7,20 @@ use Drupal\Core\Database\Query\Merge as QueryMerge;
 /**
  * Oracle implementation of \Drupal\Core\Database\Query\Merge.
  */
-class Merge extends QueryMerge {}
+class Merge extends QueryMerge {
+
+  /**
+   * {@inheritdoc}
+   */
+  function execute() {
+    // Workaround a core bug: A field set in an expression should not be set in
+    // the update as well.
+    if ($this->expressionFields) {
+      foreach ($this->expressionFields as $field => $data) {
+        unset($this->updateFields[$field]);
+      }
+    }
+
+    return parent::execute();
+  }
+}
